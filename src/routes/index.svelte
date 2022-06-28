3259942b2633fed9b2cf69a75c2e991dee8d2d47
williampersson <william@prsn.se>
2022-06-28 19:47:08 +0200

    initial

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+  <title>Mc-parkeringar</title>
+</svelte:head>
+<div class="flex h-screen justify-center items-start">
+    <div class="h-max bg-white bg-opacity-80 m-6 p-6">
+        <h1 class="text-4xl font-bold">Välkommen till mc-parkeringar.se</h1>
+        <div class="pt-10">
+            <p>Inom kort kommer här att finnas en karta över mc-parkeringar i hela sverige.</p>
+            <p>Huvudsyftet är att göra det lättare för mc-förare att hitta mc-parkeringar.</p>            
+        </div>
+    </div>
+</div>
