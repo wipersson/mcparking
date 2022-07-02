85f6c0bd00764c08ea741067ecc9b054e6e590ec
williampersson <william@prsn.se>
2022-07-02 17:31:48 +0200

    Fixing build issues.

--- src/lib/leafletMap.svelte
+++ src/lib/leafletMap.svelte
@@ -1,24 +1,24 @@
 <script>
-    import { onMount } from 'svelte';
-    import { browser } from '$app/env';
+    // import { onMount } from 'svelte';
+    // import { browser } from '$app/env';
 
-    onMount(async () => {
-        if(browser) {
-            const leaflet = await import('leaflet');
+    // onMount(async () => {
+    //     if(browser) {
+    //         const leaflet = await import('leaflet');
 
-            const map = leaflet.map('map').setView([59.610927, 16.542393], 13);
+    //         const map = leaflet.map('map').setView([59.610927, 16.542393], 13);
 
-            leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
-            let markers = [[59.608761, 16.546065], [59.610927, 16.542393], [59.603247, 16.551152]]
-            markers.forEach(element => {
-                leaflet.marker(element).addTo(map)
-                .bindPopup('En parkeringsplats!')
-            });
+    //         leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //             attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //         }).addTo(map);
+    //         let markers = [[59.608761, 16.546065], [59.610927, 16.542393], [59.603247, 16.551152]]
+    //         markers.forEach(element => {
+    //             leaflet.marker(element).addTo(map)
+    //             .bindPopup('En parkeringsplats!')
+    //         });
             
-        }
-    });
+    //     }
+    // });
 </script>
 
 
